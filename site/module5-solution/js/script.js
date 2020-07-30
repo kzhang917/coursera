3317cd6ef8bb4df4c9b249063e33015ea4af6726
Kevin Zhang <furinyxx@gmail.com>
2020-07-29 21:00:26 -0500

    Fixed script.js

--- site/module5-solution/js/script.js
+++ site/module5-solution/js/script.js
@@ -116,6 +116,8 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
+
+      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
       console.log(homeHtmlToInsertIntoMainPage);
 
